ed3d9953ff32bd61f0348f6e8d85ad5225499f7f
Pyae Sone <pyaesone@Pyaes-MacBook-Air.local>
2020-02-27 09:53:34 +0630

    Add Book

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+use Validator;
+
+class BookController extends Controller
+{
+    public function index(){
+        $books = Book::all();
+        return $books;
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required | min:5',
+            'decreption' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+        $book = new Book();
+        $book->name = $request->name;
+        $book->decreption = $request->decreption;
+        $book->save();
+
+        return 'Book Create Successfully';
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required | min:5',
+            'decreption' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+        $book = Book::find($id);
+        $book->name = $request->name;
+        $book->decreption = $request->decreption;
+        $book->save();
+
+        return $book;
+    }
+    public function show($id)
+    {
+        $book = Book::find($id);
+        if (is_null($book)) {
+            return 'Book not Found';
+        }
+        return $book;
+    }
+    public function destroy(Book $book,$id)
+    {   
+        $book = Book::find($id);
+        $book->delete();
+        return "delete Success";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/v1/login', 'UsersController@login');
+Route::post('/v1/register', 'UsersController@register');
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::post('/login', 'UsersController@login');
-    Route::post('/register', 'UsersController@register');
-    Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
+Route::group([
+    'prefix' => 'v1',
+    'middleware' => 'auth:api'
+], function () {
+    Route::get('/logout', 'UsersController@logout');
+    Route::post('/book/create','BookController@store');
+    Route::post('/book/update/{id}','BookController@update');
+    Route::post('/book/delete/{id}','BookController@destroy');
+    Route::get('/book/{id}','BookController@show');
+    Route::get('/book','BookController@index');
 });
